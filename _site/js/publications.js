8477a8af61b4c32044b1efb0b5ed42d6c6ff615f
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-03-19 21:37:49 -0700

    hey

---
+++ _site/js/publications.js
@@ -0,0 +1,18 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'publications.bib', true);
+xhr.onreadystatechange = function() {
+	if (this.readyState === 4 && this.status === 200) {
+		var parser = new BibtexParser();
+		parser.setInput(this.responseText);
+		parser.bibtex();
+
+		var ul = document.getElementById("publications");
+		for (var i = 0; i < parser.entries.length; i++) {
+			var entry = parser.entries[i];
+			var li = document.createElement("li");
+			li.innerHTML = entry.entryTags.title + ". " + entry.entryTags.journal + ", " + entry.entryTags.year;
+			ul.appendChild(li);
+		}
+	}
+};
+xhr.send();
